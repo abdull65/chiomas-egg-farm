9946a43fd18f95479ecb309b15df4462e87f1da4
Abdullahi Mohammed <odanwu211@gmail.com>
2023-04-27 17:28:01 +0100

    update/adding sticky position to the navigation bar

--- javascript/index.js
+++ javascript/index.js
@@ -34,3 +34,27 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+// Sticky navigation
+const indexSectionElEl = document.querySelector("#index_section");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(indexSectionElEl);
